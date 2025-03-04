3db6938ffdbae7cd6844099490388f9ec4532b85
drosas91 <drosasarevalo@gmail.com>
2025-03-03 23:36:21 -0300

    Implement dagger di configuration

---
+++ app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.pokedex.di
+
+import com.example.pokedex.api.PokemonListApi
+import com.example.pokedex.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideApi(): PokemonListApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PokemonListApi::class.java)
+
+    }
+}
